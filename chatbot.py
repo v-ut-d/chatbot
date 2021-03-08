3da7714da65d07cfe5a84bddb76de39630db4a9c
PolinaBalanyeva <polina.balanyeva@gmail.com>
2021-03-08 15:37:16 +0900

    train reduced

--- chatbot.py
+++ chatbot.py
@@ -37,8 +37,7 @@ bot = ChatBot(
 trainer = ChatterBotCorpusTrainer(bot)
 
 trainer.train(
-    "chatterbot.corpus.japanese",
-    "chatterbot.corpus.english"
+    "chatterbot.corpus.japanese"
 )
 
 @client.event
