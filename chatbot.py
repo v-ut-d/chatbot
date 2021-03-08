21c56083fead3376e6f8dcfdf684df4b44c44c9e
PolinaBalanyeva <polina.balanyeva@gmail.com>
2021-03-08 18:38:48 +0900

    auto removing old data

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,25 @@ import os
 # Uncomment the following lines to enable verbose logging
 import logging
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import psycopg2
+import urlparse
+from urllib.parse import urlparse
 
+result = urlparse(os.environ['DATABASE_URL'])
+username = result.username
+password = result.password
+database = result.path[1:]
+hostname = result.hostname
+port = result.port
+connection = psycopg2.connect(
+    database = database,
+    user = username,
+    password = password,
+    host = hostname,
+    port = port
+)
+cur = connection.cursor()
+cur.execute("DELETE FROM statement WHERE created_at < (now() - '10 days'::interval);")
 # import spacy
 # nlp = spacy.load("xx_sent_ud_sm")
 logging.basicConfig(level=logging.INFO)
